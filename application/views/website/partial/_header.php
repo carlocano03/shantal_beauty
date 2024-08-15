b95ce0da735fa88133a5ab4a4ef56dbc143270e9
Jake <62888790+jakecastor09@users.noreply.github.com>
2024-08-15 10:01:14 +0800

    Setup SEO

--- application/views/website/partial/_header.php
+++ application/views/website/partial/_header.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description"
+        content="To have a growing Church that demonstrate True Christian Community And To Lead People into Life Changing Relationship with Jesus Christ.">
+    <meta property="og:title" content="Change Life Christian Church" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://changelifechristianchurch.org/" />
     <title><?= $title?></title>
     <link rel="icon" type="image/png" href="<?= base_url('assets/images/clc.png')?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
